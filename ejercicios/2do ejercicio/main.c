1c99f3b2e633908133f5b87d20d84cd58f2942c7
RoosebeltHiciano <133432984+RoosebeltHiciano@users.noreply.github.com>
2023-05-25 17:11:22 -0300

    ejercicios.

---
+++ ejercicios/2do ejercicio/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Aplicación de operadores. */
+int main(void)
+{
+    int i = 15, j, k, l;
+
+    j = (15 > i--) > (14 < ++i);
+    printf("\nEl valor de j es: %d", j);
+
+    i = 15; // Restablecer el valor de i
+
+    k = !('b' != 'd') > (!i - 1);
+    printf("\nEl valor de k es: %d", k);
+
+    l = (! (34 > (70 % 2))) || 0;
+    printf("\nEl valor de l es: %d", l);
+
+    return 0;
+}
